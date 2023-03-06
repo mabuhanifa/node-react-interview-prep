7f86af22b00bda4e7a8f47201d01801155c6d116
shourovahmd <moh.abuhanifa@gmail.com>
2023-03-06 23:47:02 +0600

    counter

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -1,9 +1,14 @@
-import './App.css';
+import { useState } from "react";
+import "./App.css";
 
 function App() {
+  const [state, setState] = useState(0);
+
   return (
     <div>
-      app
+      <h2>{state}</h2>
+      <button onClick={() => setState((prev) => prev + 1)}>+</button>
+      <button onClick={() => setState((prev) => prev - 1)}>-</button>
     </div>
   );
 }
