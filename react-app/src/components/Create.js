47d2416be5f2d52a757e1012f6808f76472ddce3
shourovahmd <moh.abuhanifa@gmail.com>
2023-03-08 22:15:31 +0600

    createElement with props

--- react-app/src/components/Create.js
+++ react-app/src/components/Create.js
@@ -3,5 +3,6 @@ import React from "react";
 export const List = () => {
   return <div>list</div>;
 };
+
 export const Create = ({name}) =>
   React.createElement("div", { className: "create" }, `Hello ${name}`);
