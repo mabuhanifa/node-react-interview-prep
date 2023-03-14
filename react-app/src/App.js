70f953fae9ed9ab2b78f123b34834524b29ce02d
shourovahmd <moh.abuhanifa@gmail.com>
2023-03-14 16:12:32 +0600

    withCounter

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -1,17 +1,10 @@
 import "./App.css";
-import CallBack from "./hooks/CallBack";
 
 function App() {
   
-  // const {loading,data,error} = useFetch('https://jsonplaceholder.typicode.com/todos');
-  
   return (
    <div>
-    {/* <Counter/>
-    <List/>
-    <Create name={"Shourov"}/>
-    <Ref/> */}
-    <CallBack/>
+    
    </div>
   );
 }
---
+++ react-app/src/HOC/withCounter.js
@@ -0,0 +1,26 @@
+import React from "react";
+
+const withCounter = (OriginalComponent) => {
+  class NewComponent extends React.Component {
+    state = {
+      count: 0,
+    };
+
+    incrementCount = () => {
+      this.setState((prevState) => ({ count: prevState.count + 1 }));
+    };
+
+    render() {
+      return (
+        <OriginalComponent
+          count={this.state.count}
+          incrementCount={this.incrementCount}
+        />
+      );
+    }
+  }
+
+  return <NewComponent />;
+};
+
+export default withCounter;
