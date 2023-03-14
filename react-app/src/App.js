fe04ee5638ed485869e28710d6190f5090640952
shourovahmd <moh.abuhanifa@gmail.com>
2023-03-14 17:16:02 +0600

    HoverCounter

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -1,9 +1,15 @@
 import "./App.css";
+import ClickCounter from "./render-prop/ClickCounter";
+import Counter from "./render-prop/Counter";
 
 function App() {
   return (
     <div>
-      
+      <Counter>
+        {(counter, incrementCount) => (
+          <ClickCounter count={counter} incrementCount={incrementCount} />
+        )}
+      </Counter>
     </div>
   );
 }
---
+++ react-app/src/render-prop/HoverCounter.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function HoverCounter({ count, incrementCount }) {
+  return (
+    <div>
+      <h1 onMouseOver={incrementCount}>Hovered {count} times</h1>
+    </div>
+  );
+}
