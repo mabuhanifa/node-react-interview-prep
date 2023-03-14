ece326d9cc6e01aa20028dc40e2bb243a326219b
shourovahmd <moh.abuhanifa@gmail.com>
2023-03-14 13:17:31 +0600

    callback setUp

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -1,5 +1,5 @@
 import "./App.css";
-import Memo from "./hooks/Memo";
+import CallBack from "./hooks/CallBack";
 
 function App() {
   
@@ -11,7 +11,7 @@ function App() {
     <List/>
     <Create name={"Shourov"}/>
     <Ref/> */}
-    <Memo/>
+    <CallBack/>
    </div>
   );
 }
--- react-app/src/hooks/CallBack.js
+++ react-app/src/hooks/CallBack.js
@@ -1,7 +1,37 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
 
 export default function CallBack() {
+  const [number, setNumber] = useState(1);
+
+  const [dark, setDark] = useState(false);
+
+  const themeStyle = {
+    backgroundColor: dark ? "black" : "white",
+    color: dark ? "white" : "black",
+    padding: "20px",
+  };
+  const getItems = () => {
+    return [number, number + 1, number + 2];
+  };
   return (
-    <div>CallBack</div>
-  )
+    <div style={themeStyle}>
+      <input
+        type="number"
+        value={number}
+        onChange={(e) => setNumber(Number(e.target.value))}
+      />
+      <button onClick={() => setDark((t) => !t)}> Change Theme</button>
+      <List getItems={getItems} />
+    </div>
+  );
+}
+
+function List({ getItems }) {
+  const [items, setItems] = useState([]);
+  useEffect(() => {
+    setItems(getItems());
+    console.log(`getting items`);
+  }, [getItems]);
+
+  return items.map((item) => <div key={item}>{item}</div>);
 }
