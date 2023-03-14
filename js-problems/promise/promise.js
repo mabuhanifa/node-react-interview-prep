a267c2b8f66d5bfb1d5f163808064c2683d4f8c1
shourovahmd <moh.abuhanifa@gmail.com>
2023-03-14 23:48:25 +0600

    myPromise

---
+++ js-problems/promise/promise.js
@@ -0,0 +1,14 @@
+/* The Promise object represents the eventual completion (or failure)
+ of an asynchronous operation and its resulting value 
+ */
+
+const myPromise = new Promise((resolve, reject) => {
+  setTimeout(() => {
+    resolve("foo");
+  }, 300);
+});
+
+myPromise
+  .then(handleFulfilledA, handleRejectedA)
+  .then(handleFulfilledB, handleRejectedB)
+  .then(handleFulfilledC, handleRejectedC);
