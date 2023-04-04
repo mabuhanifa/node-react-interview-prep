39c7ee692fcd69e5327eddd51567b250d68aa921
shourovahmd <moh.abuhanifa@gmail.com>
2023-04-05 00:44:16 +0600

    changed

--- js-problems/dsa/bigO.js
+++ js-problems/dsa/bigO.js
@@ -3,5 +3,4 @@ Big O Notation is used in computer science to analyse the performance of an algo
 
 Big O specifically looks at the worst-case scenario of an algorithm – looking at the big picture. It tells us how long a function will take to execute (execution time) or how much space (e.g., in memory or disk) the function takes up as the input to that function approaches infinity (i.e. becomes very large).
 
-
 */
