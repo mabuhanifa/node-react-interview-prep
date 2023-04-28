e8a0daac3f1d325f3aea9ffae218641b987040c4
shourovahmd <moh.abuhanifa@gmail.com>
2023-04-29 00:09:26 +0600

    classes

---
+++ js-problems/classes/class.js
@@ -0,0 +1,3 @@
+/*
+Classes are a template for creating objects. They encapsulate data with code to work on that data. Classes in JS are built on prototypes but also have some syntax and semantics that are unique to classes.
+*/
